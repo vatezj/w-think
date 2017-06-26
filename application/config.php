26945193168c410b9ebb881769ebcc6f7e78a85b
vate <2324757255@qq.com>
2017-06-26 18:13:01 +0800

    动态图标

--- application/config.php
+++ application/config.php
@@ -141,7 +141,7 @@ return [
     // +----------------------------------------------------------------------
 
     // 异常页面的模板文件
-    'exception_tmpl'         => THINK_PATH . 'tpl' . DS . 'think_exception.tpl',
+    'exception_tmpl'         => THINK_PATH . 'tpl' . DS . '404.tpl',
 
     // 错误显示信息,非调试模式有效
     'error_message'          => '页面错误！请稍后再试～',
